3fac45fadfdcd2f6cdca3237a15eba6b3761da63
Simon AM <purrcat259@users.noreply.github.com>
2017-10-04 22:06:26 +0200

    Added basic API key authentication

--- server/api.py
+++ server/api.py
@@ -6,9 +6,22 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 # socketio = SocketIO(app)
 
+debug_mode = True
+api_header_name = 'API-KEY'
+api_key = 'test'
+if not debug_mode:
+    api_key = 'bla'
+
 
 def get_connection():
     return r.connect(host='localhost', port=28015, db='test')
+
+
+@app.before_request
+def check_api_key():
+    request_api_key = request.headers.get(api_header_name)
+    if not api_key == request_api_key:
+        abort(401)
 
 
 @app.route('/')
@@ -25,4 +38,4 @@ def receive_event():
 
 if __name__ == '__main__':
     # socketio.run(app)
-    app.run(debug=True)
+    app.run(debug=debug_mode)
--- server/test/test_sending_request.py
+++ server/test/test_sending_request.py
@@ -4,7 +4,12 @@ import requests
 
 url = 'http://127.0.0.1:5000'
 event_url = url + '/event'
-headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain', 'API-KEY': 'test'}
+
+
+def test_sending_without_api_key():
+    response = requests.post(url=event_url)
+    assert 401 == response.status_code
 
 
 def test_sending_valid_json_request():
