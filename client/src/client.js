839337f5843a0776430acfa3abc3e97a0488f3f9
Simon AM <simon@agius-muscat.net>
2017-10-05 00:33:13 +0200

    Added basic client socket code

--- client/src/client.js
+++ client/src/client.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 // Init the socket
-let socket = io();
+let socket = io('http://localhost:5000/test');
 
 // Setup the socket
 socket.on('connect_failed', () => {
@@ -10,4 +10,11 @@ socket.on('connect_failed', () => {
 
 socket.on('disconnect', () => {
     socket.close();
+});
+
+socket.on('connect', () => {
+    socket.emit('my event', { data: 'hello' });
+    socket.on('my response', (res) => {
+        console.log(res);
+    });
 });
