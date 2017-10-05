995b56566bc3c73ca8959a5913648a3b143baa44
Simon AM <purrcat259@users.noreply.github.com>
2017-10-05 14:02:31 +0200

    Moved watch function into class

--- app/journal_watcher/journal_watcher.py
+++ app/journal_watcher/journal_watcher.py
@@ -7,25 +7,27 @@ def get_difference(a, b):
     return [x for x in b if x not in s]
 
 
-def watch_file(file_path, delay=0.1):
-    with open(file_path, 'r') as log_file:
-        # Go to the end of the file
-        log_file.seek(0, 2)
-        while True:
-            line = log_file.readline()
-            if line and not line == '\n':
-                yield line
-            time.sleep(delay)
-
-
 class JournalWatcher:
-    def __init__(self, directory):
+    def __init__(self, directory, watch_delay=0.1):
         self._directory = directory
+        self._watch_delay = watch_delay
         self._journal_files = os.listdir(directory)
-        self._current_file = None
+        self._current_file_path = None
 
-    def watch(self):
-        pass
+    def watch_file(self):
+        with open(self._current_file_path, 'r') as log_file:
+            # Go to the end of the file
+            log_file.seek(0, 2)
+            while True:
+                # stop looping if a new journal has been detected
+                new_file_path = self.get_new_journal_file()
+                if new_file_path:
+                    self._current_file_path = new_file_path
+                    break
+                line = log_file.readline()
+                if line and not line == '\n':
+                    yield line
+                time.sleep(self._watch_delay)
 
     def get_new_journal_file(self):
         files = os.listdir(self._directory)
